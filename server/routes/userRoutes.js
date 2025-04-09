b7b2b91c415ebf8374dba5e6ee4207a0aad541d2
Maya <mayaawwad003@gmail.com>
2025-04-09 11:03:25 +0300

    refactor: reorder userController and userRoutes functions to match and rename getProfile to getUserProfile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,10 @@ import express from 'express';
 import {
   registerUser,
   loginUser,
-  getAllUsers,
+  getUserProfile,
   updateUser,
   deleteUser,
-  getProfile,
+  getAllUsers,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { adminOnly } from '../middleware/adminMiddleware.js';
@@ -14,24 +14,8 @@ import { selfOrAdmin } from '../middleware/selfOrAdmin.js';
 // Create a new Express router instance
 const router = express.Router();
 
-// Protected Routes (require login)
-// @route   GET /api/users/profile
-// @desc    Get all users
-router.route('/').get(protect, adminOnly, getAllUsers);
-
-// @route   DELETE /api/users/profile
-// @desc    Delete logged-in user's profile
-router.delete('/profile', protect, selfOrAdmin, deleteUser);
-
-// @route   Patch /api/users/profile
-// @desc    Update logged-in user's profile
-router.patch('/profile', protect, updateUser);
-
-// @route   GET /api/users/profile
-// @desc    Get logged-in user's profile
-router.get('/profile', protect, getProfile);
-
 // Public Routes
+
 // @route   POST /api/users/register
 // @desc    Register a new user
 router.post('/register', registerUser);
@@ -39,6 +23,24 @@ router.post('/register', registerUser);
 // @route   POST /api/users/login
 // @desc    Login and return token
 router.post('/login', loginUser);
+
+// Protected Routes (require login)
+
+// @route   GET /api/users/profile
+// @desc    Get logged-in user's profile
+router.get('/profile', protect, getUserProfile);
+
+// @route   Patch /api/users/profile
+// @desc    Update logged-in user's profile
+router.patch('/profile', protect, updateUser);
+
+// @route   DELETE /api/users/profile
+// @desc    Delete logged-in user's profile
+router.delete('/profile', protect, selfOrAdmin, deleteUser);
+
+// @route   GET /api/users/profile
+// @desc    Get all users
+router.route('/').get(protect, adminOnly, getAllUsers);
 
 // Export the router
 export default router;
