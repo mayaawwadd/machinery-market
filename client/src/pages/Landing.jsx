6e31b7a39897f2232fe204f635e0ba241c79236d
mayaawwadd <mayaawwad003@gmail.com>
2025-05-05 00:20:51 +0300

    feat: finalize features section layout and content in Landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/landing/Hero'
+import Features from '../components/landing/Features'
 
 function Landing() {
   return (
     <>
       <Hero></Hero>
+      <Features></Features>
     </>
   );
 }
