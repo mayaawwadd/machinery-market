4555d9575a43330948726ee41ad0c6f2bae080f3
mayaawwadd <mayaawwad003@gmail.com>
2025-05-05 12:38:47 +0300

    feat: implement interactive 'How It Works' section with tabs and vertical stepper

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/landing/Hero'
 import Features from '../components/landing/Features'
+import HowItWorks from '../components/landing/HowItWorks';
 
 function Landing() {
   return (
     <>
       <Hero></Hero>
       <Features></Features>
+      <HowItWorks></HowItWorks>
     </>
   );
 }
